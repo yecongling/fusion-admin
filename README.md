8f411f835f89487b9311cbb094a4cc929d8672e0
yeconglin <499475142@qq.com>
2024-10-23 22:34:57 +0800

    ✨ feat: 添加左边侧边栏、封装axios请求对象

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Install the dependencies:
 
 ```bash
-pnpm install
+yarn install
 ```
 
 ## Get Started
@@ -13,17 +13,17 @@ pnpm install
 Start the dev server:
 
 ```bash
-pnpm dev
+yarn dev
 ```
 
 Build the app for production:
 
 ```bash
-pnpm build
+yarn build
 ```
 
 Preview the production build locally:
 
 ```bash
-pnpm preview
+yarn preview
 ```
--- src/layouts/LeftMenu/index.tsx
+++ src/layouts/LeftMenu/index.tsx
@@ -1,12 +1,114 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { Layout, Image, Spin, Menu, MenuProps, Button, Tooltip } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, setCollapse } from "@stores/store.ts";
+import favicon from "@assets/svg/vite.svg";
+import { Link, useLocation } from "react-router-dom";
+
+import "./leftMenu.scss";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+
+type MenuItem = Required<MenuProps>["items"][number];
 
 /**
  * 左边的菜单栏
  */
 const LeftMenu: React.FC = memo(() => {
-    return (
-        <></>
-    )
+  // 从状态库中获取状态
+  const globalState = useSelector((state: RootState) => state.global);
+  const { menus, theme, collapse } = globalState;
+  const dispatch = useDispatch();
+
+  const { pathname } = useLocation();
+
+  // 定义一些状态变量
+  const [menuList, setMenuList] = useState<MenuItem[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([pathname]);
+  const [openKeys, setOpenKeys] = useState<string[]>([]);
+
+  const titleColor = theme === "dark" ? "#fff" : "#1890ff";
+  return (
+    <Layout.Sider
+      trigger={null}
+      collapsedWidth={48}
+      className="scroll ant-menu"
+      style={{
+        overflowX: "hidden",
+        zIndex: 999,
+        borderRight: "1px solid #e9edf0",
+      }}
+      collapsible
+      theme={theme}
+      collapsed={collapse}
+    >
+      <div className="dis-fl jc-sb ai-ct toolbox">
+        <Link to="/" style={{ width: "100%" }}>
+          <div
+            className="hd-64 mgr-01 dis-fl ai-ct jc-ct"
+            style={{ justifyContent: "space-around" }}
+          >
+            <Image width={25} src={favicon} preview={false} />
+            {collapse ? (
+              ""
+            ) : (
+              <p
+                style={{
+                  fontWeight: "bold",
+                  margin: "0 12px",
+                  fontSize: "20px",
+                  color: titleColor,
+                }}
+              >
+                FusionAdmin
+              </p>
+            )}
+          </div>
+        </Link>
+      </div>
+      <Spin wrapperClassName="side-menu" spinning={loading} tip="加载中">
+        <Menu
+          mode="inline"
+          theme={theme}
+          selectedKeys={selectedKeys}
+          openKeys={openKeys}
+          items={menuList}
+        />
+      </Spin>
+      <div
+        className="collapse"
+        style={{
+          display: "flex",
+          justifyContent: collapse ? "center" : "end",
+        }}
+      >
+        <Button
+          type="text"
+          size="small"
+          style={{
+            cursor: "pointer",
+            fontSize: "16px",
+          }}
+          onClick={() => dispatch(setCollapse(!collapse))}
+          className="btnbor"
+        >
+          {collapse ? (
+            <Tooltip title="展开">
+              <MenuUnfoldOutlined
+                style={{ color: theme === "dark" ? "white" : "black" }}
+              />
+            </Tooltip>
+          ) : (
+            <Tooltip title="收起">
+              <MenuFoldOutlined
+                style={{ color: theme === "dark" ? "white" : "black" }}
+              />
+            </Tooltip>
+          )}
+        </Button>
+      </div>
+    </Layout.Sider>
+  );
 });
 
 export default LeftMenu;
