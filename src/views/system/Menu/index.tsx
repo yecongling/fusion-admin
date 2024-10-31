06560bdb10faef9041c8cea2fec574e0f1a28044
ycl <499475142@qq.com>
2024-10-31 17:03:23 +0800

    ✨ feat: 继续完善系统菜单管理页面

--- src/views/system/Menu/index.tsx
+++ src/views/system/Menu/index.tsx
@@ -1,3 +1,16 @@
+import { RedoOutlined, SearchOutlined } from '@ant-design/icons';
+import {
+  Button,
+  Card,
+  Col,
+  ConfigProvider,
+  Flex,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+} from 'antd';
 import React from 'react';
 
 /**
@@ -5,6 +18,90 @@ import React from 'react';
  * @returns
  */
 const Menu: React.FC = () => {
-  return <>系统菜单维护</>;
+  const [form] = Form.useForm();
+
+  /**
+   * 检索表单提交
+   * @param values  检索表单条件
+   */
+  const onFinish = (values: any) => {};
+
+  return (
+    <Flex vertical flex="auto">
+      {/* 菜单检索条件栏 */}
+      <ConfigProvider
+        theme={{
+          components: {
+            Form: {
+              itemMarginBottom: 0,
+            },
+          },
+        }}
+      >
+        <Card>
+          <Form
+            form={form}
+            initialValues={{ menu_type: '', status: '' }}
+            onFinish={onFinish}
+          >
+            <Row gutter={24}>
+              <Col span={6}>
+                <Form.Item name="title" label="菜单名称" colon={false}>
+                  <Input autoFocus allowClear autoComplete="off" />
+                </Form.Item>
+              </Col>
+              <Col span={6}>
+                <Form.Item name="menu_type" label="菜单类型" colon={false}>
+                  <Select
+                    allowClear
+                    options={[
+                      { value: '', label: '请选择', disabled: true },
+                      { value: 1, label: '一级菜单' },
+                      { value: 2, label: '子菜单' },
+                      { value: 3, label: '按钮权限' },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={6}>
+                <Form.Item name="status" label="状态" colon={false}>
+                  <Select
+                    allowClear
+                    options={[
+                      { value: '', label: '请选择', disabled: true },
+                      { value: 1, label: '启用' },
+                      { value: 0, label: '停用' },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={6} style={{ textAlign: 'right' }}>
+                <Space>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    icon={<SearchOutlined />}
+                  >
+                    检索
+                  </Button>
+                  <Button
+                    type="default"
+                    icon={<RedoOutlined />}
+                    onClick={() => {
+                      form.resetFields();
+                    }}
+                  >
+                    重置
+                  </Button>
+                </Space>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </ConfigProvider>
+      {/* 查询表格 */}
+      <Card style={{ flex: 1, marginTop: '8px' }}>查询表格</Card>
+    </Flex>
+  );
 };
 export default Menu;
