da45622bc7927709f17fb2d4afb6e1a843dbd971
ycl <499475142@qq.com>
2024-11-25 12:00:43 +0800

    ✨ feat(项目): 端点、端点类型
    添加端点维护、端点类型维护模块

---
+++ src/views/project/Endpoint/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+/**
+ * 端点管理模块
+ * @returns 
+ */
+const Endpoint: React.FC = () => {
+    return (
+        <>端点管理</>
+    )
+}
+export default Endpoint;
---
+++ src/views/project/EndpointType/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+/**
+ * 端点类型维护模块
+ */
+const EndpointType: React.FC = () => {
+    return (
+        <div>
+            EndpointType
+        </div>
+    )
+}
+export default EndpointType;
